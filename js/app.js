aa95ec2b1dac3fa67c31fc21fd665c0555f6aab7
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2020-12-19 22:52:19 +0100

    Adds do while loop to prompt user for random number to guess.

--- js/app.js
+++ js/app.js
@@ -6,7 +6,16 @@ function getRandomNumber(upper) {
   return Math.floor( Math.random() * upper ) + 1;
 }
 
+do {
+  guess = prompt("Guess a number");
+} while ( parseInt(guess) !== randomNumber );
+
+main.innerHTML = `<h1>Yes it was ${randomNumber}</h1>`;
+
+
+
+
 // TODO: Use a loop to create a number guessing game
-//  1) Ask the user to enter a number and assign that value to the `guess` variable
+//  1) Ask the user to enter a number and assign that value to the `guess` variable - DONE
 //  2) End the loop when the user's guess matches the random number
 //  3) Display a message letting the user know they guessed the number
